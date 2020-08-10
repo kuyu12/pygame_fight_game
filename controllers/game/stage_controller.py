10773a0fa4ec4e1bc0b474c040d4485b839e8296
Yair Hadad <kuyu12@gmail.com>
2020-08-10 14:49:33 +0300

    - Add stage road map controller

--- controllers/game/stage_controller.py
+++ controllers/game/stage_controller.py
@@ -1,9 +1,12 @@
+import datetime
 import uuid
+import random
 
 from controllers.game.collision_controller import CollisionController
 from controllers.game.player_controller import PlayerController
 from controllers.surface_controller import SurfaceController
 from controllers.game.sprite_controller import SpriteController
+from enums.screen_type import ScreenType
 from model.game_state import GameState
 from utils.const import USER_INFO_SIZE
 from utils.json_mappers.stage_json_mapper import SignalMapper
@@ -14,7 +17,6 @@ from views.sprite_views.enemy_sprite import EnemySprite
 from views.sprite_views.user_sprite import UserSprite
 from utils.logger import logger
 from pydispatch import dispatcher
-import random
 
 
 class StageController(SurfaceController):
@@ -23,11 +25,12 @@ class StageController(SurfaceController):
         logger.info("StageController is Created")
 
         # Stage Data
+        self.start_stage_time = datetime.datetime.now()
         self.stage_data = stage_data
         self.player_data = player_data
         self.game_state = GameState(self.player_data)
-
         self.user_sprite = UserSprite(self.stage_data, self.player_data, self.game_state.user_state.user_id)
+        self.finish_wave = True
 
         # controllers
         self.sprite_controller = SpriteController(self.stage_data.background, self.user_sprite)
@@ -41,17 +44,8 @@ class StageController(SurfaceController):
         # events
         dispatcher.connect(self.handle_collision_event, signal=SignalMapper.COLLISION_UPDATE, sender=dispatcher.Any)
 
-        # TEMP
-        # for temp in range(4):
-        #     enemy_data = UtilsFactory.get_player_data('Grey')
-        #     enemy_id = uuid.uuid4()
-        #     self.game_state.add_enemy(enemy_data, enemy_id)
-        #     self.enemy = EnemySprite((random.randint(100, 900), 600), stage_data.background.size, enemy_data,
-        #                              self.user_sprite, enemy_id)
-        #     self.sprite_controller.add(self.enemy)
-        # END TEMP
-
     def update(self):
+        self.stage_update()
         self.sprite_controller.update()
         self.collision_controller.update()
         self.user_info_view.update(self.game_state)
@@ -76,3 +70,35 @@ class StageController(SurfaceController):
     def handle_collision_event(self, event):
         self.game_state.handle_collision_event(event)
         self.sprite_controller.handle_collision_event(event)
+
+    def add_enemy(self,name):
+        enemy_data = UtilsFactory.get_player_data(name)
+        enemy_id = uuid.uuid4()
+        self.game_state.add_enemy(enemy_data, enemy_id)
+        enemy = EnemySprite((random.randint(100, 900), 600), self.stage_data.background.size, enemy_data,
+                                 self.user_sprite, enemy_id)
+        self.sprite_controller.add(enemy)
+
+    def stage_update(self):
+        if self.finish_wave and len(self.game_state.enemies) == 0:
+            self.finish_wave = False
+            self.start_stage_time = datetime.datetime.now()
+
+        pass_time = datetime.datetime.now() - self.start_stage_time
+        if pass_time > datetime.timedelta(seconds = 6) and len(self.game_state.enemies) == 0:
+            if len(self.stage_data.road_map) == 0:
+                print("Stage is end ")
+                dispatcher.send(signal=SignalMapper.SCREEN_TYPE_CHANGE,
+                                event=ScreenType.MENU)
+                return
+
+            current_enemeis = self.stage_data.road_map.pop(0)
+            print(current_enemeis)
+            for enemy_dict in current_enemeis:
+                for key in enemy_dict:
+                    [self.add_enemy(enemy_dict[key]) for num in range(int(key))]
+            self.finish_wave = True
+
+
+
+
